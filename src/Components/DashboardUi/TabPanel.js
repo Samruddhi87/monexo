3317a5f7f6d391e72de6b03befe08fbe333b012f
Samruddhi Arekar <101266857+Samruddhi87@users.noreply.github.com>
2022-06-20 15:50:29 +0530

    Add files via upload

--- src/Components/DashboardUi/TabPanel.js
+++ src/Components/DashboardUi/TabPanel.js
@@ -14,8 +14,8 @@ import EnhancedTable from './EnhancedTable';
 import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined';
 import SyncOutlinedIcon from '@mui/icons-material/SyncOutlined';
 import {Button} from "@material-ui/core";
-import {useEffect } from "react";
-
+import {useEffect,useState,useContext } from "react";
+import { AppBar } from '@material-ui/core';
 import "./TabPanel.scss";
 import { MarginOutlined, MiscellaneousServicesOutlined } from '@mui/icons-material';
 import { CSVLink  } from "react-csv";
@@ -24,13 +24,18 @@ import axios from 'axios';
 import env from '../../enviorment.json';
 import { useSelector } from "react-redux";
 import Mis from './Mis';
+import Delqunitel from './Delqunitel';
+import Wip from './Wip';
+import UserRole from '../LoginComponent/Login';
+import {UserRoleContext} from '../../App'
+import Reffered from './Reffered';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-  const childFunc = React.useRef(null)
- 
+  const childFunc = React.useRef(EnhancedTable)
 
   return (
+    
     <div
       role="tabpanel"
       hidden={value !== index}
@@ -61,10 +66,14 @@ function a11yProps(index) {
 }
 
 export default function BasicTabs(props) {
-  const userRole = useSelector((state) => state.authRedux.userRole)
+  // const userRole = useSelector((state) => state.userRole)
+  // const ur=useContext(UserRole);
   const token = useSelector((state)=>state.authRedux.token)
   const [value, setValue] = React.useState(0);
   const [data, setData] = React.useState([]);
+    const inputUserRole=useContext(UserRoleContext)
+   const userRole=inputUserRole.state.inputRole
+    // console.log("tabpanel", userRole.state.inputRole);
   // const childFunc = React.useRef(null)
 
   // React.useEffect(() => {
@@ -73,7 +82,7 @@ export default function BasicTabs(props) {
   // parentToChild = () =>{
   //   setData()
   // }
-  
+  // const userRole=useContext(UserRoleContext)
   let headers = [
     { label: "Cust ID", key: "customer_id" },
     { label: "Cust Name", key: "customer_name" },
@@ -89,11 +98,13 @@ export default function BasicTabs(props) {
   
 
   const handleChange = (event, newValue) => {
+
     setValue(newValue);
   };
   const colorhandle ={ 
    color : "#2A9134"
   }
+
 
   const customTheme = createTheme({
     palette: {
@@ -126,148 +137,215 @@ export default function BasicTabs(props) {
         }
       }
     }
-  });
+  }); 
+  
+  
+//************************************************** */
+//inputuserrole = 1(dynamic) 
+//************************************************** */
+ // const handleSysncHandler = async() =>{
+  //   try {
+  //     await axios.get(env.apiUrl + 'api/users/sync-my_report',
+  //         {
+  //             headers: { "Authorization": `Bearer ${token}` }
 
-  const handleSysncHandler = async() =>{
-    try {
-      await axios.get(env.apiUrl + 'api/users/sync-my_report',
-          {
-              headers: { "Authorization": `Bearer ${token}` }
-
-          }).then(res => {
-              // console.log("demo url" + res.data.response.response)
+  //         }).then(res => {
+  //             // console.log("demo url" + res.data.response.response)
           
-          })
+  //         })
 
-      // } 
-  } catch (error) {
-      console.log(error)
-  }
-  }
-  // useEffect(() =>{
-  //   console.log(localStorage.getItem("token"))
-  //   console.log("use effect works tabpanel")
-  //   const dashboard = {
-  //     search: props.searchVal,
-  //     startdate: props.startDate,
-  //     endDate :props.endDate
-  //   };
-  //   console.log(dashboard)
-  // },[])
+  //     // } 
+  // } catch (error) {
+  //     console.log(error)
+  // }
+  // }
+  useEffect(() =>{
+    // console.log(localStorage.getItem("token"))
+    // console.log("use effect works tabpanel")
+    // const dashboard = {
+    //   search: props.searchVal,
+    //   startdate: props.startDate,
+    //   endDate :props.endDate
+    // };
+    // console.log(dashboard)
+  },[])
 
-  return (
-
-   
+  return (                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     <Box sx={{ width: '100%' }}>
-        {/* <ThemeProvider theme={customTheme}>
-          <div className={classes.root}>
-          <AppBar position="static" elevation={9}> */}
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
-                mat-stretch-tabs
-                textColor="secondary"
-                indicatorColor="secondary"
-                TabIndicatorProps={{
-                  style: {
-                    backgroundColor: "#2A9134"
-                   }
-                  }}
-               
-                 inkbarstyle={{background: 'red'}}
-        >
-            { userRole ==  3 &&
-          <Tab className="new "label="Work In Progress" {...a11yProps(0)} />
-            }
-          { userRole ==  4 &&
-          <Tab className="new"label="Referred" {...a11yProps(0)} />
-}
-          <Tab className="new"label="Downgrade" {...a11yProps(1)} />
-          <Tab className="new"label="Approved/Disbursed" {...a11yProps(2)} />
-          <Tab className="new"label="Pending Disbursed" {...a11yProps(2)} />
-          <Tab className="new"label="MIS Report" {...a11yProps(2)} />
-          {/* <CSVDownload  target="_blank" />; */}
-  
-          <Button style={{maxWidth: '30px', borderRadius: '8px', maxHeight: '50px', minWidth: '23px', minHeight: '50px', paddingRight:'28px',left:'490px',background:'#2A9134',color:'white'}} variant="contained"  endIcon={<SyncOutlinedIcon />} 
-          onClick={handleSysncHandler}> </Button>
-          <CSVLink data={data}  headers={headers} style={{ minHeight: '28px', marginLeft:'500px',background:'#2A9134',color:'white',
-           borderRadius: '8px',padding:'10px'}} variant="contained"> 
-            <span style={{paddingTop:'12px'}}><ArrowDownwardOutlinedIcon /></span> 
-          </CSVLink>
-           
-        </Tabs>
-        { userRole ==  1 &&
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
-                mat-stretch-tabs
-                textColor="secondary"
-                indicatorColor="secondary"
-                TabIndicatorProps={{
-                  style: {
-                    backgroundColor: "#2A9134"
-                   }
-                  }}
-               
-                 inkbarstyle={{background: 'red'}}
-        >
-          <Tab className="new "label="Work In Progress" {...a11yProps(0)} />
 
-          <Tab className="new"label="Referred" {...a11yProps(0)} />
-
-          <Tab className="new"label="Downgrade" {...a11yProps(1)} />
-          <Tab className="new"label="Approved/Disbursed" {...a11yProps(2)} />
-          <Tab className="new"label="Pending Disbursed" {...a11yProps(2)} />
-          {/* <CSVDownload  target="_blank" />; */}
-  
-          <Button style={{maxWidth: '30px', borderRadius: '8px', maxHeight: '50px', minWidth: '23px', minHeight: '50px', paddingRight:'28px',left:'490px',background:'#2A9134',color:'white'}} variant="contained"  endIcon={<SyncOutlinedIcon />} 
-          onClick={handleSysncHandler}> </Button>
-          <CSVLink data={data}  headers={headers} style={{ minHeight: '28px', marginLeft:'500px',background:'#2A9134',color:'white',
-           borderRadius: '8px',padding:'10px'}} variant="contained"> 
-            <span style={{paddingTop:'12px'}}><ArrowDownwardOutlinedIcon /></span> 
-          </CSVLink>
-           
-        </Tabs>
-}
-        
-      <TabPanel value={value} index={0}>
-      { userRole ==  4 &&
-      <Referred startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-}
 { userRole ==  3 &&
-      <EnhancedT startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      <Downgrade startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-      <AppDisb startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <PendingDisbursed startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        <Mis startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-      </TabPanel>
-     { userRole ==  1 &&
-     <div>
-     <TabPanel value={value} index={0}>
-     <EnhancedT startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-     </TabPanel>
-       <TabPanel value={value} index={0}>
-       <Referred startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-       </TabPanel>
-     <TabPanel value={value} index={1}>
-     <Downgrade startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-     </TabPanel>
-     <TabPanel value={value} index={2}>
-     <AppDisb startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-     </TabPanel>
-     <TabPanel value={value} index={3}>
-       <PendingDisbursed startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
-     </TabPanel>
-     </div>
+    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
+            mat-stretch-tabs
+            textColor="secondary"
+            indicatorColor="secondary"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "#2A9134"
+               }
+              }}
+           
+             inkbarstyle={{background: 'red'}}
+    >
+    
+     
+           <Tab className="new "label="Work In Progress" {...a11yProps(0)} />
+          <Tab className="new"label="Downgrade" {...a11yProps(2)} />
+          <Tab className="new"label="Approved/Disbursed" {...a11yProps(3)} />
+          <Tab className="new"label="Pending Disbursed" {...a11yProps(4)} />
+
+          
+<Button style={{maxWidth: '30px', borderRadius: '8px', maxHeight: '50px', minWidth: '23px', minHeight: '50px', paddingRight:'28px',left:'490px',background:'#2A9134',color:'white'}} variant="contained"  endIcon={<SyncOutlinedIcon />} 
+      > </Button>
+      <CSVLink data={data}  headers={headers} style={{ minHeight: '28px', marginLeft:'500px',background:'#2A9134',color:'white',
+       borderRadius: '8px',padding:'10px'}} variant="contained"> 
+        <span style={{paddingTop:'12px'}}><ArrowDownwardOutlinedIcon /></span> 
+      </CSVLink>
+       
+</Tabs>   
+
+  }      
+            
+     { userRole ==  3 &&
+    
+  <div> 
+    <TabPanel value={value} index={0}>
+  <Wip startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+  </TabPanel>
+  <TabPanel value={value} index={1} >
+  <Downgrade startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+  </TabPanel>
+  <TabPanel value={value} index={2}>
+  <AppDisb startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+  </TabPanel>
+  <TabPanel value={value} index={3}>
+    <PendingDisbursed startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+  </TabPanel>
+  </div>
 }
 
-     
-      </Box>
+
+
+{ userRole ==  4 &&
+<Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
+            mat-stretch-tabs
+            textColor="secondary"
+            indicatorColor="secondary"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "#2A9134"
+               }
+              }}
+           
+             inkbarstyle={{background: 'red'}}
+    >
+  
+  
+
+      {/* <Tab className="new"label="Referred" {...a11yProps(0)} /> */}
+      {/* <Tab className="new"label="Downgrade" {...a11yProps(1)} />
+      <Tab className="new"label="Approved/Disbursed" {...a11yProps(2)} />
+      <Tab className="new"label="Pending Disbursed" {...a11yProps(3)} /> */}
     
-  );
+
+    
+      <Tab className="new"label="Referred" {...a11yProps(1)} />
+      <Tab className="new"label="Downgrade" {...a11yProps(2)} />
+      <Tab className="new"label="Approved/Disbursed" {...a11yProps(3)} />
+      <Tab className="new"label="Pending Disbursed" {...a11yProps(4)} /> 
+
+      <Button style={{maxWidth: '30px', borderRadius: '8px', maxHeight: '50px', minWidth: '23px', minHeight: '50px', paddingRight:'28px',left:'490px',background:'#2A9134',color:'white'}} variant="contained"  endIcon={<SyncOutlinedIcon />} 
+      > </Button>
+      <CSVLink data={data}  headers={headers} style={{ minHeight: '28px', marginLeft:'500px',background:'#2A9134',color:'white',
+       borderRadius: '8px',padding:'10px'}} variant="contained"> 
+        <span style={{paddingTop:'12px'}}><ArrowDownwardOutlinedIcon /></span> 
+      </CSVLink>
+         </Tabs>
+} 
+
+
+  
+  { userRole ==  4 &&
+  <div>  
+ <TabPanel value={value} index={0}>
+    <Referred startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+    </TabPanel>
+    <TabPanel value={value} index={1}>
+    <Downgrade startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+    </TabPanel>
+    <TabPanel value={value} index={2}>
+    <AppDisb startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+    </TabPanel>
+    <TabPanel value={value} index={3}>
+      <PendingDisbursed startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+    </TabPanel> 
+
+   </div>
 }
+
+
+{ userRole ==  1 &&
+    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
+            mat-stretch-tabs
+            textColor="secondary"
+            indicatorColor="secondary"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: "#2A9134"
+               }
+              }}
+           
+             inkbarstyle={{background: 'red'}}
+    >
+       
+      <Tab className="new "label="Work In Progress" {...a11yProps(0)} />
+      <Tab className="new"label="Referred" {...a11yProps(1)} />
+      <Tab className="new"label="Downgrade" {...a11yProps(1)} />
+      <Tab className="new"label="Approved/Disbursed" {...a11yProps(3)} />
+      <Tab className="new"label="Pending Disbursed" {...a11yProps(4)} />
+      <Tab className="new"label="MIS Report" {...a11yProps(4)} />
+     
+   
+  
+      <Button style={{maxWidth: '30px', borderRadius: '8px', maxHeight: '50px', minWidth: '23px', minHeight: '50px', paddingRight:'28px',left:'490px',background:'#2A9134',color:'white'}} variant="contained"  endIcon={<SyncOutlinedIcon />} 
+      > </Button>
+      <CSVLink data={data}  headers={headers} style={{ minHeight: '28px', marginLeft:'500px',background:'#2A9134',color:'white',
+       borderRadius: '8px',padding:'10px'}} variant="contained"> 
+      <span style={{paddingTop:'12px'}}><ArrowDownwardOutlinedIcon /></span> 
+      </CSVLink>
+     
+    </Tabs>
+    
+            }
+ { userRole ==  1 &&
+ <div>
+
+<TabPanel value={value} index={0}>
+ <Wip startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+ </TabPanel>
+   <TabPanel value={value} index={1}>
+   <Referred startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+   </TabPanel>
+ <TabPanel value={value} index={2}>
+ <Downgrade startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+ </TabPanel>
+ <TabPanel value={value} index={3}>
+ <AppDisb startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+ </TabPanel>
+ <TabPanel value={value} index={4}>
+   <PendingDisbursed startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+ </TabPanel>
+ <TabPanel value={value} index={5}>
+   <Mis startDate={props.startDate} endDate={props.endDate} searchVal={props.searchVal} setData={setData} />
+ </TabPanel>
+ </div>
+}
+
+
+
+  </Box>
+ 
+)
+
+
+}
+
